504ae30c39db227f87896af8b8127dc19e6c0fae
Mr. Brown <gitcherrypick.work@gmail.com>
2023-08-25 09:49:54 +0500

    added observer example with heat index

--- HF examples/observer.rb
+++ HF examples/observer.rb
@@ -78,9 +78,39 @@ class CurrentConditionsDisplay < DisplayElement
   end
 end
 
+class HeatIndexDisplay < DisplayElement
+  attr_reader :heat_index
+
+  def initialize(weather_data)
+    @weather_data = weather_data
+    weather_data.register_observer(self)
+  end
+
+  def update(temp, rh, pressure)
+    @heat_index = compute_heat_index(temp, rh)
+    display_message
+  end
+
+  def compute_heat_index(t, rh)
+    ((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) +
+    (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) +
+    (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+    (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+      (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) +
+      (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+      0.000000000843296 * (t * t * rh * rh * rh)) -
+      (0.0000000000481975 * (t * t * t * rh * rh * rh)))
+  end
+
+  def display_message
+    puts "Heat index is: #{heat_index}\n\n"
+  end
+end
+
 class WeatherStation
   weather_data = WeatherData.new
-  current_display = CurrentConditionsDisplay.new(weather_data)
+  CurrentConditionsDisplay.new(weather_data)
+  HeatIndexDisplay.new(weather_data)
 
   # statistics_display = StatisticsDisplay.new(weather_data)
   # forecast_display = ForecastDisplay(weather_data)
