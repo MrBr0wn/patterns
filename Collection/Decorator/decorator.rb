3272218f3ae494cd7364bb0706c54555338af936
Mr. Brown <gitcherrypick.work@gmail.com>
2023-08-31 23:05:44 +0500

    chore: replace tabs with spaces

--- Collection/Decorator/decorator.rb
+++ Collection/Decorator/decorator.rb
@@ -1,43 +1,45 @@
+# frozen_string_literal: true
+
 class Component
-	def operation
-		raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
-	end
+  def operation
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
 end
 
 class ConcreteComponent < Component
-	def operation
-		'ConcreteComponent'
-	end
+  def operation
+    'ConcreteComponent'
+  end
 end
 
 class Decorator < Component
-	attr_accessor :component
+  attr_accessor :component
 
-	def initialize(component)
-		@component = component
-	end
+  def initialize(component)
+    @component = component
+  end
 
-	def operation
-		@component.operation
-	end
+  def operation
+    @component.operation
+  end
 end
 
 class ConcreteDecoratorA < Decorator
-	def operation
-		"ConcreteDecoratorA(#{@component.operation})"
-	end
+  def operation
+    "ConcreteDecoratorA(#{@component.operation})"
+  end
 end
 
 class ConcreteDecoratorB < Decorator
-	def operation
-		"ConcreteDecoratorB(#{@component.operation})"
-	end
+  def operation
+    "ConcreteDecoratorB(#{@component.operation})"
+  end
 end
 
 def client_code(component)
-	# ...
-	print "RESULT: #{component.operation}"
-	# ...
+  # ...
+  print "RESULT: #{component.operation}"
+  # ...
 end
 
 simple = ConcreteComponent.new
